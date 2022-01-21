d13d9d49651812186841884bf4e8499ee9d425b0
Our2020Project <suraj.yadav97@outlook.com>
2022-01-21 16:08:11 +0530

    Component Creation By Shashi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
+import {LoginComponent} from './components/login/login.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {FooterComponent} from './components/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  { path: 'login', component: LoginComponent },  
+  { path: 'signup', component: SignupComponent },  
+  { path: 'header', component: HeaderComponent }, 
+  { path: 'dashboard', component: DashboardComponent }, 
+  { path: 'footer', component: FooterComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
