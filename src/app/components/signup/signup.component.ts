4907df44cf2e45d7090a4e0f6b825079c54585f1
Our2022Project <suraj.yadav97@outlook.com>
2022-02-04 16:54:59 +0530

    Signup Page by Suraj99

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
+import { EmailValidator, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -22,9 +22,13 @@ export class SignupComponent implements OnInit {
 
   initializeForm(): void {
     this.SignupForm = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      userName: ['', Validators.required],
+      email: ['', Validators.required,Validators.email],
+      Createpassword: ['', Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})')],
+      Confirmpassword: ['', Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})')],
+      Address1: ['', Validators.required],
+      Address2: ['', Validators.required],
+      City: ['', Validators.required],
+      ZIP: ['', Validators.required,Validators.maxLength(16)],
     });
   }
 
