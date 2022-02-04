96c5de356de4c2febbd543d662de65106a5e6465
Our2020Project <suraj.yadav97@outlook.com>
2022-02-04 14:34:57 +0530

    header moibile friendly by suraj98.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,8 +8,13 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   constructor(private router: Router) { }
-
+  showDroplink1: boolean = false;
+  showDroplink2: boolean = false;
+  showDroplink3: boolean = false;
+  showDroplink4: boolean = false;
   check: boolean = false;
+  searchBox: boolean = false;
+
   ngOnInit(): void {
   }
 
@@ -18,13 +23,50 @@ export class HeaderComponent implements OnInit {
     this.router.navigateByUrl(`/footer`);
   }
 
+  search(){ 
+    if(this.searchBox == true){
+    this.searchBox = false;
+  }else{
+    this.searchBox = true;
+  }
+}
+
   onclick() {
     if(this.check == true){
       this.check = false;
     }else{
       this.check = true;
+    } 
+  }
+
+
+  dropBtn1(){
+    if(this.showDroplink1 == true){
+      this.showDroplink1 = false;
+    }else{
+      this.showDroplink1 = true;
     }
-    
+  }
+  dropBtn2(){
+    if(this.showDroplink2 == true){
+      this.showDroplink2 = false;
+    }else{
+      this.showDroplink2 = true;
+    }
+  }
+  dropBtn3(){
+    if(this.showDroplink3 == true){
+      this.showDroplink3 = false;
+    }else{
+      this.showDroplink3 = true;
+    }
+  }
+  dropBtn4(){
+    if(this.showDroplink4 == true){
+      this.showDroplink4 = false;
+    }else{
+      this.showDroplink4 = true;
+    }
   }
 }
 
