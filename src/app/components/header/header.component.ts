a057d1d9ea5e8a8a43d0334b6d8ef614f1fdf83c
Our2020Project <suraj.yadav97@outlook.com>
2022-02-08 11:01:57 +0530

    header dropdown remove and replace brand with logo in header and footer by suraj98.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,10 +8,6 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   constructor(private router: Router) { }
-  showDroplink1: boolean = false;
-  showDroplink2: boolean = false;
-  showDroplink3: boolean = false;
-  showDroplink4: boolean = false;
   check: boolean = false;
   searchBox: boolean = false;
 
@@ -37,37 +33,7 @@ export class HeaderComponent implements OnInit {
     }else{
       this.check = true;
     } 
-  }
-
-
-  dropBtn1(){
-    if(this.showDroplink1 == true){
-      this.showDroplink1 = false;
-    }else{
-      this.showDroplink1 = true;
-    }
-  }
-  dropBtn2(){
-    if(this.showDroplink2 == true){
-      this.showDroplink2 = false;
-    }else{
-      this.showDroplink2 = true;
-    }
-  }
-  dropBtn3(){
-    if(this.showDroplink3 == true){
-      this.showDroplink3 = false;
-    }else{
-      this.showDroplink3 = true;
-    }
-  }
-  dropBtn4(){
-    if(this.showDroplink4 == true){
-      this.showDroplink4 = false;
-    }else{
-      this.showDroplink4 = true;
-    }
-  }
+  } 
 }
 
 
