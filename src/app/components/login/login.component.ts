a0fc6054e8d475d0fa1a5962abbbe2fbade1ad2a
Our2022Project <suraj.yadav97@outlook.com>
2022-02-09 18:33:05 +0530

    login page Email and Password Dynamic validation by Suraj99

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -18,8 +18,8 @@ export class LoginComponent {
 
   initializeForm(): void {
     this.loginForm = this.fb.group({
-      userName: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
-      password: ['', [Validators.required, Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')]],
+      userName: ['', [Validators.required, Validators.pattern('^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$')]],
+      password: ['', [Validators.required, Validators.minLength(8),Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')]],
     });
   }
 
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -24,12 +24,14 @@ export class SignupComponent implements OnInit {
     this.SignupForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
       password: ['', [Validators.required, Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')]],
-      ConfirmPassword: ['', [Validators.required, Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')]],
+      ConfirmPassword: [null, [Validators.required, ]],
       Address1: ['', Validators.required],
       Address2: ['', Validators.required],
       City: ['', Validators.required],
       ZIP: ['', Validators.required],
-    });
+      Phonenumber:[null,Validators.required,Validators.maxLength(10),Validators.minLength(10),Validators.pattern(/^(\+\d{1,3}[- ]?)?\d{10}$/)]
+    }
+    );
   }
 
   goToLogin() {
