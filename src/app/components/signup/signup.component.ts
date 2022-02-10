cf95b034314df60d132bbe123d38bc4641fdcdcf
Our2020Project <suraj.yadav97@outlook.com>
2022-02-10 23:36:38 +0530

    signup password and confirm password matched by suraj98.

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -27,7 +27,7 @@ export class SignupComponent implements OnInit {
     this.SignupForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       password: ['', [Validators.required, Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')]],
-      ConfirmPassword: [null, [Validators.required,]],
+      ConfirmPassword: ['', [Validators.required, Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')]],
       Phonenumber: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
       Address1: ['', [Validators.required, Validators.minLength(10)]],
       Address2: ['', Validators.required],
