5d6fab6748030fe5e0c5430cd7df17ba60afff04
Our2022Project <suraj.yadav97@outlook.com>
2022-02-10 14:16:28 +0530

    Dashboard Background images and signin Vaildation by Suraj99

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -13,7 +13,7 @@ export class SignupComponent implements OnInit {
   constructor(private router: Router, private fb: FormBuilder) { }
 
   SignupForm: any;
-  
+
   fieldTextType: boolean = false;
   fieldType: boolean = false;
 
@@ -27,25 +27,25 @@ export class SignupComponent implements OnInit {
     this.SignupForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       password: ['', [Validators.required, Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')]],
-      ConfirmPassword: [null, [Validators.required, ]],
+      ConfirmPassword: [null, [Validators.required,]],
       Phonenumber: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
-      Address1: ['', Validators.required],
+      Address1: ['', Validators.required, Validators.minLength(10)],
       Address2: ['', Validators.required],
       City: ['', Validators.required],
-      ZIP: ['', Validators.required],
+      ZIP: ['', Validators.required,],
     }
     );
   }
-  
+
   toggleFieldTextType() {
     this.fieldTextType = !this.fieldTextType;
   }
-  toggleTextType(){
+  toggleTextType() {
     this.fieldType = !this.fieldType;
   }
   goToLogin() {
     this.router.navigateByUrl('/login');
   }
-  
+
 
 }
