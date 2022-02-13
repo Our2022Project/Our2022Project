bf2fa58970848a48e78bceb8614e902f06cd3a34
Our2020Project <suraj.yadav97@outlook.com>
2022-02-14 02:13:57 +0530

    show rate ui with hide and show option in rate page by suraj98.

--- src/app/components/rate-tranist/rate-tranist.component.ts
+++ src/app/components/rate-tranist/rate-tranist.component.ts
@@ -11,7 +11,8 @@ export class RateTranistComponent implements OnInit {
 
   constructor(private router: Router, private fb: FormBuilder) { }
   checkRateForm: any;
-  
+  drop: boolean = false;
+  check: boolean = false;
 
   ngOnInit(): void {
     this.initializeForm();
@@ -31,6 +32,14 @@ export class RateTranistComponent implements OnInit {
   }
   checkRate() {
     this.router.navigateByUrl('/signup');
+  }
+  dropbtn(){
+    this.drop = !this.drop;
+    if(this.check == true){
+      this.check = false;
+    }else{
+      this.check = true;
+    } 
   }
   
 }
